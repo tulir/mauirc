eb688f6a57199ae9484552cb313a5ba662dac309
Tulir293 <tulir293@maunium.net>
2016-03-19 18:08:42 +0200

    Many updates, much features, wow.

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,7 @@
 import {bootstrap}    from 'angular2/platform/browser';
-import {AppComponent} from './app.component';
+import {HTTP_PROVIDERS} from 'angular2/http';
+import {MessagePanelComponent} from './message-panel.component';
+import {MessageFormComponent} from './message-form.component';
 
-bootstrap(AppComponent);
+bootstrap(MessagePanelComponent);
+bootstrap(MessageFormComponent, [HTTP_PROVIDERS]);
---
+++ app/message-panel.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core'
+
+@Component({
+  selector: 'message-panel',
+  template: ''
+})
+
+export class MessagePanelComponent { }
