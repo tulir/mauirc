a7fd046a57569db7d7226e438e6294db6c49eb2b
Tulir293 <tulir293@maunium.net>
2016-03-20 15:02:15 +0200

    Yay! It works!

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
-var socket = new WebSocket('ws://127.0.0.1/socket');
+var socket = null
 var connected = false
 
-socket.onopen = function() {
-  console.log("Connected!")
-  $("#nocon").addClass("hidden-xs-up")
-  $('#message-send').removeAttr('disabled')
-  $('#message-text').removeAttr('disabled')
-  connected = true
-};
+function connect() {
+  console.log("Connecting to socket...")
+  socket = new WebSocket('ws://127.0.0.1/socket');
 
-socket.onmessage = function (evt) {
-  evt.data = JSON.parse(evt.data)
-  receive(evt.data.network, evt.data.channel, evt.data.timestamp, evt.data.sender, evt.data.command, evt.data.message)
-};
+  socket.onopen = function() {
+    console.log("Connected!")
+    $("#nocon").addClass("hidden-xs-up")
+    $('#message-send').removeAttr('disabled')
+    $('#message-text').removeAttr('disabled')
+    connected = true
+  };
 
-socket.onclose = function() {
-  console.log("Disconnected!")
-  $("#nocon").removeClass("hidden-xs-up")
-  $('#message-send').attr('disabled', true)
-  $('#message-text').attr('disabled', true)
-};
+  socket.onmessage = function (evt) {
+    var data = JSON.parse(evt.data)
+    receive(data.network, data.channel, data.timestamp, data.sender, data.command, data.message)
+  };
+
+  socket.onclose = function() {
+    connected = false
+    $("#nocon").removeClass("hidden-xs-up")
+    $('#message-send').attr('disabled', true)
+    $('#message-text').attr('disabled', true)
+
+    setTimeout(function() {
+      connect();
+    }, 5000)
+    console.log("Disconnected!")
+  };
+}
 
 function history(n){
   $.ajax({
@@ -28,9 +38,8 @@ function history(n){
     contentType: "application/json; charset=utf-8",
     success: function(data){
       data = JSON.parse(data)
-      for (var i = data.length + 1; i > 0; i++)
-      data.forEach(function(val, i, array) {
-        receive(data[i].network, data[i].channel, data[i].timestamp, data[i].sender, data[i].command, data[i].message)
+      data.reverse().forEach(function(val, i, arr) {
+        receive(val.network, val.channel, val.timestamp, val.sender, val.command, val.message)
       })
     },
     error: function(jqXHR, textStatus, errorThrown) {
@@ -43,7 +52,6 @@ function history(n){
 
 function receive(network, channel, timestamp, sender, command, message){
   if (command == "privmsg") {
-    console.log(moment(timestamp * 1000).format("HH:mm DD.MM.YYYY") + " || " + channel + "@" + network + " || <" + sender + "> " + message);
     $("#messages").loadTemplate("message.html", {
       sender: sender,
       date: moment(timestamp * 1000).format("HH:mm:ss DD.MM.YYYY"),
@@ -71,3 +79,4 @@ function send(){
 }
 
 history(10)
+connect();
