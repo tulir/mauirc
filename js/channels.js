346aebc819ee0a1ddb4bfaa6e484814382967d7e
Tulir293 <tulir293@maunium.net>
2016-03-30 22:47:40 +0300

    Move userlist stuff to userlist.js

--- js/channels.js
+++ js/channels.js
@@ -105,37 +105,6 @@ function openChannel(network, channel) {
   }, {append: true, isFile: false, async: false})
 }
 
-function updateUserList(){
-  if (channelData[getActiveNetwork()] !== undefined && channelData[getActiveNetwork()][getActiveChannel()] !== undefined) {
-    if (isUserListHidden() && !wasUserListHiddenManually()) toggleUserList(false)
-
-    $("#userlist").text("")
-    channelData[getActiveNetwork()][getActiveChannel()].userlist.forEach(function(val, i, arr){
-      $("#userlist").append(val + "<br>")
-    })
-  } else if (!isUserListHidden()) toggleUserList(false)
-}
-
-function isUserListHidden() {
-  return $("#userlist").hasClass("hidden")
-}
-
-function wasUserListHiddenManually() {
-  return $("#userlist").hasClass("hidden-manual")
-}
-
-function toggleUserList(manual) {
-  if ($("#userlist").hasClass("hidden")) {
-    $("#userlist").removeClass("hidden")
-    $("#userlist").removeClass("hidden-manual")
-    $("#messaging").removeClass("messaging-userlisthidden")
-  } else {
-    $("#userlist").addClass("hidden")
-    if(manual) $("#userlist").addClass("hidden-manual")
-    $("#messaging").addClass("messaging-userlisthidden")
-  }
-}
-
 function switchView() {
   if ($("#networks").hasClass("hidden-tablet-down")) {
     $("#messaging").addClass("hidden-tablet-down")
