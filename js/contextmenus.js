d2d34ea44eaf876afa28c73bedad728ae037c07c
Tulir293 <tulir293@maunium.net>
2016-04-20 14:58:48 +0300

    Added PM link context menu

--- js/contextmenus.js
+++ js/contextmenus.js
@@ -16,39 +16,21 @@
 
 $(function() {
   $.contextMenu({
+    selector: '.pm-link',
+    callback: ctxUserList,
+    items: {
+      query: {name: "Open Query", icon: "query", disabled: false},
+      whois: {name: "Whois", icon: "whois", disable: true},
+      op: {name: "Give OP", icon: "op", disabled: true},
+      deop: {name: "Take OP", icon: "deop", disabled: true},
+      kick: {name: "Kick", icon: "kick", disabled: true},
+      ban: {name: "Ban", icon: "ban", disabled: true}
+    }
+  });
+
+  $.contextMenu({
     selector: '.message',
-    callback: function(key, options) {
-      if (key === "delete") {
-        sendMessage({
-          type: "delete",
-          id: $(this).parent().attr("id").slice("msgwrap-".length),
-        })
-      } else if (key === "copy") {
-        var element = $(this).find(".message-text")
-        if (element.length === 0) {
-          element = $(this).find(".clipboard-data")
-          if (element.length === 0) {
-            console.log("Clipboard data not found!")
-            return
-          }
-        }
-        var wasHidden = false
-        if (element.hasClass("hidden")) {
-          wasHidden = true
-          element.removeClass("hidden")
-        }
-        var selection = window.getSelection()
-        var range = document.createRange()
-        range.selectNodeContents(element[0])
-        selection.removeAllRanges()
-        selection.addRange(range)
-        document.execCommand("copy")
-        selection.removeAllRanges()
-        if (wasHidden) {
-          element.addClass("hidden")
-        }
-      }
-    },
+    callback: ctxMessage,
     items: {
       delete: {name: "Delete Message", icon: "delete"},
       copy: {name: "Copy Text", icon: "copy"}
@@ -57,37 +39,78 @@ $(function() {
 
   $.contextMenu({
     selector: '.channel-switcher',
-    callback: function(key, options) {
-      var idPieces = $(this).attr("id").split("-")
-
-      if (key === "delete") {
-        sendMessage({
-          type: "clear",
-          network: idPieces[1],
-          channel: idPieces[2]
-        })
-      } else if (key === "part") {
-        if(idPieces[2].startsWith("#")) {
-          sendMessage({
-            type: "message",
-            network: idPieces[1],
-            channel: idPieces[2],
-            command: "part",
-            message: "Leaving"
-          })
-        } else {
-          sendMessage({
-            type: "close",
-            network: idPieces[1],
-            channel: idPieces[2]
-          })
-        }
-        closeChannel(idPieces[1], idPieces[2])
-      }
-    },
+    callback: ctxChanSwitcher,
     items: {
       delete: {name: "Clear History", icon: "delete"},
       part: {name: "Part Channel", icon: "quit"}
     }
   });
 });
+
+function ctxUserList(key, options) {
+  if (key === "query") {
+    $(this).click()
+  }
+}
+
+function ctxMessage(key, options) {
+  if (key === "delete") {
+    sendMessage({
+      type: "delete",
+      id: $(this).parent().attr("id").slice("msgwrap-".length),
+    })
+  } else if (key === "copy") {
+    var element = $(this).find(".message-text")
+    if (element.length === 0) {
+      element = $(this).find(".clipboard-data")
+      if (element.length === 0) {
+        console.log("Clipboard data not found!")
+        return
+      }
+    }
+    var wasHidden = false
+    if (element.hasClass("hidden")) {
+      wasHidden = true
+      element.removeClass("hidden")
+    }
+    var selection = window.getSelection()
+    var range = document.createRange()
+    range.selectNodeContents(element[0])
+    selection.removeAllRanges()
+    selection.addRange(range)
+    document.execCommand("copy")
+    selection.removeAllRanges()
+    if (wasHidden) {
+      element.addClass("hidden")
+    }
+  }
+}
+
+function ctxChanSwitcher(key, options) {
+  var idPieces = $(this).attr("id").split("-")
+
+  if (key === "delete") {
+    sendMessage({
+      type: "clear",
+      network: idPieces[1],
+      channel: idPieces[2]
+    })
+  } else if (key === "part") {
+    if(idPieces[2].startsWith("#")) {
+      sendMessage({
+        type: "message",
+        network: idPieces[1],
+        channel: idPieces[2],
+        command: "part",
+        message: "Leaving"
+      })
+    } else {
+      sendMessage({
+        type: "close",
+        network: idPieces[1],
+        channel: idPieces[2]
+      })
+    }
+    closeChannel(idPieces[1], idPieces[2])
+  }
+}
