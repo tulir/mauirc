79e924ce43f65113e325c4ab5bef2101f7cf7287
Tulir293 <tulir293@maunium.net>
2016-07-24 23:16:06 +0300

    Apparently global "use strict" is bad for you

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"use strict"
 function snOpenScriptEditor(net, scripts) {
   "use strict"
 	$("#settings-main").addClass("hidden")
