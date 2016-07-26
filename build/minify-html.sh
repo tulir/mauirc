c247de159da4c0f2e437406cb9a7cf6c4b81202f
Tulir293 <tulir293@maunium.net>
2016-07-26 22:52:52 +0300

    Use loop instead of manual file listing

--- build/minify-html.sh
+++ build/minify-html.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-htmlminify() {
-    html-minifier --html5 \
+for template in templates/*.html; do
+    echo -n '<script type="text/html" id="template-'$template'"> ' >> templates.min.html
+    cat $template | html-minifier --html5 \
         --collapse-boolean-attributes \
         --collapse-inline-tag-whitespace \
         --collapse-whitespace \
@@ -9,42 +10,9 @@ htmlminify() {
         --remove-redundant-attributes \
         --remove-script-type-attributes \
         --remove-style-link-type-attributes \
-        --use-short-doctype
-}
-
-minifyappend() {
-    echo -n '<script type="text/html" id="template-'$1'"> ' >> templates.min.html
-    cat "templates/$1.html" | htmlminify >> templates.min.html
+        --use-short-doctype >> templates.min.html
     echo -n "</script>" >> templates.min.html
-}
-
-minifyappend-multi() {
-    cat "templates/$1.html" | htmlminify >> templates.min.html
-}
-
-echo > templates.min.html
-minifyappend error
-minifyappend network
-minifyappend channel
-minifyappend action
-minifyappend channel-adder
-minifyappend channel-switcher
-minifyappend login
-minifyappend main
-minifyappend settings
-minifyappend script-list-entry
-minifyappend message
-minifyappend message-preview-both
-minifyappend message-preview-image
-minifyappend message-preview-text
-minifyappend network-switcher
-minifyappend whois
-minifyappend invite
-minifyappend rawio
-minifyappend userlist-entry
-minifyappend userlist-invite
-minifyappend userlist-invite-box
-minifyappend oper
+done
 
 if [ ! -f index.max.html ]; then
     mv index.html index.max.html
