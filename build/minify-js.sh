b97fac4da73a2a3855bfa3517fa4cf760d0ef0d7
Tulir293 <tulir293@maunium.net>
2016-07-27 11:57:44 +0300

    Use loop in JS minifier

--- build/minify-js.sh
+++ build/minify-js.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
-cat \
-    ./js/lib/linkify.min.js \
-    ./js/lib/linkify-html.min.js \
-    ./js/lib/linkify-jquery.min.js \
-    ./js/data.js \
-    ./js/vars.js \
-    ./js/util.js \
-    ./js/channels.js \
-    ./js/autocomplete.js \
-    ./js/userlist.js \
-    ./js/messaging.js \
-    ./js/contextmenus.js \
-    ./js/settings.js \
-    ./js/modal.js \
-    ./js/whois.js \
-    ./js/oper.js \
-    ./js/rawio.js \
-    ./js/scripts.js \
-    ./js/mauirc.js \
-    ./js/auth.js \
-    ./js/keybinds.js \
-    ./js/load.js \
-    > ./index.js
+for script in
+    lib/linkify.min lib/linkify-html.min \
+    data vars util channels \
+    autocomplete userlist \
+    messaging contextmenus \
+    settings modal whois oper \
+    rawio scripts mauirc auth \
+    keybinds load
+do cat js/$script >> index.js done
 
 uglifyjs -cmo index.min.js -- index.js
