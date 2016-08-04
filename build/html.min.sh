716310241e3a34d8829856e42e4d0aa6492453f7
Tulir293 <tulir293@maunium.net>
2016-08-04 17:14:54 +0300

    Rename html/ to templates/

--- build/html.min.sh
+++ build/html.min.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo > templates.html
 for template in html/*.gohtml; do
-    templatename=`echo $template | awk '{ print substr($1, 6, length($1) - 12 ) }'`
+    templatename=`echo $template | awk '{ print substr($1, 11, length($1) - 17 ) }'`
     echo -n '<script type="text/html" class="template" id="template-'$templatename'"> ' >> templates.html
     cat $template | html-minifier --html5 \
         --collapse-boolean-attributes \
--- build/html.sh
+++ build/html.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo > templates.html
-for template in html/*.gohtml; do
-    templatename=`echo $template | awk '{ print substr($1, 6, length($1) - 12 ) }'`
+for template in templates/*.gohtml; do
+    templatename=`echo $template | awk '{ print substr($1, 11, length($1) - 17 ) }'`
     echo -n '<script type="text/html" class="template" id="template-'$templatename'">' >> templates.html
     cat $template | html-minifier --html5 --remove-comments | sed "s/^/  /g" >> templates.html
     echo -e "</script>\n" >> templates.html
