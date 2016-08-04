7dd3dd033aa416d6aec90e770b207974b4b19737
Tulir293 <tulir293@maunium.net>
2016-08-04 18:22:46 +0300

    Don't pass non-minify packer through html minifier

--- build/html.sh
+++ build/html.sh
@@ -3,7 +3,7 @@ echo > templates.html
 for template in templates/html/*.gohtml; do
     templatename=`echo $template | awk '{ print substr($1, 16, length($1) - 22 ) }'`
     echo -n '<script type="text/html" class="template" id="template-'$templatename'">' >> templates.html
-    cat $template | html-minifier --html5 --remove-comments | sed "s/^/  /g" >> templates.html
+    cat $template | sed "s/^/  /g" >> templates.html
     echo -e "</script>\n" >> templates.html
 done
 
