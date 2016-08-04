685f1c50ff8fb2172105a9dc9991afbe81b2e5ec
Tulir293 <tulir293@maunium.net>
2016-08-04 15:34:41 +0300

    Re-add comment removing for templates

--- build/html.sh
+++ build/html.sh
@@ -3,6 +3,6 @@ echo > templates.html
 for template in templates/*.gohtml; do
     templatename=`echo $template | awk '{ print substr($1, 11, length($1) - 17 ) }'`
     echo -n '<script type="text/html" class="template" id="template-'$templatename'">' >> templates.html
-    cat $template | sed "s/^/  /g" >> templates.html
+    cat $template | html-minifier --html5 --remove-comments | sed "s/^/  /g" >> templates.html
     echo -e "</script>\n" >> templates.html
 done
