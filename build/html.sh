cdbf500de8c45047b535d8a6e960b5549875be47
Tulir293 <tulir293@maunium.net>
2016-08-04 15:04:14 +0300

    Print non-minified template pack nicely

--- build/html.sh
+++ build/html.sh
@@ -2,7 +2,7 @@
 echo > templates.html
 for template in templates/*.gohtml; do
     templatename=`echo $template | awk '{ print substr($1, 11, length($1) - 17 ) }'`
-    echo -n '<script type="text/html" class="template" id="template-'$templatename'"> ' >> templates.html
-    cat $template >> templates.html
-    echo -n "</script>" >> templates.html
+    echo -n '<script type="text/html" class="template" id="template-'$templatename'">' >> templates.html
+    cat $template | html-minifier --remove-comments | sed "s/^/  /g" >> templates.html
+    echo -e "</script>\n" >> templates.html
 done
