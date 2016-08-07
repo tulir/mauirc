ac1f4160aea7d51c85a55bd78255ca90ad25e80e
Tulir293 <tulir293@maunium.net>
2016-08-07 13:49:14 +0300

    Fix a few things

--- ui/messaging.go
+++ ui/messaging.go
@@ -187,14 +187,16 @@ func Receive(msg messages.Message, isNew bool) {
 	switch strings.ToUpper(msg.Command) {
 	case "ACTION":
 		templateData.Class += " user-action"
-		templateData.Clipboard = fmt.Sprintln("*", msg.Sender, msg.Message)
+		templateData.Clipboard = fmt.Sprintln("*", templateData.Sender, templateData.Message)
 	case irc.JOIN:
-		templateData.Clipboard = fmt.Sprintln(msg.Sender, "joined", msg.Message)
+		templateData.Message = fmt.Sprintln("joined ", templateData.Message)
+		templateData.Clipboard = fmt.Sprintln(templateData.Sender, "joined", templateData.Message)
 		templateData.Class += " secondary-action joinpart"
 	case irc.PART:
 		fallthrough
 	case irc.QUIT:
-		templateData.Clipboard = fmt.Sprintln(msg.Sender, "left: ", msg.Message)
+		templateData.Message = fmt.Sprintln("left: ", templateData.Message)
+		templateData.Clipboard = fmt.Sprintln(templateData.Sender, "left: ", templateData.Message)
 		templateData.Class += " secondary-action joinpart"
 	case irc.KICK:
 		index := strings.Index(msg.Message, ":")
