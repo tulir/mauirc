11e8a0814d56419f1856c959268122abea481fa4
Tulir293 <tulir293@maunium.net>
2016-08-10 19:54:26 +0300

    Update scripts on server

--- data/scripts.go
+++ data/scripts.go
@@ -17,6 +17,12 @@
 // Package data contains the session data storage system
 package data
 
+import (
+	"fmt"
+	"github.com/gopherjs/jquery"
+	"maunium.net/go/mauirc/util/console"
+)
+
 // ScriptStore contains functions common to stores that have a script unit
 type ScriptStore map[string]string
 
@@ -26,14 +32,57 @@ func (ss ScriptStore) Get(name string) string {
 	return script
 }
 
+// Rename the script with the given name
+func (ss ScriptStore) Rename(net, oldName, newName string, callback func()) {
+	jquery.Ajax(map[string]interface{}{
+		"type": "POST",
+		"url":  fmt.Sprintf("/script/%s/%s/", net, oldName),
+		"data": fmt.Sprintf("%s,%s", net, newName),
+		jquery.SUCCESS: func() {
+			console.Log("Successfully renamed script", oldName, "@", net, "to", newName)
+
+			ss[newName] = ss[oldName]
+			delete(ss, oldName)
+			callback()
+		},
+		jquery.ERROR: func(info map[string]interface{}, textStatus, errorThrown string) {
+			console.Error("Failed to rename script: HTTP", info["status"])
+			console.Error(info)
+		},
+	})
+}
+
 // Put the given script under the given name
-func (ss ScriptStore) Put(name, script string) {
-	ss[name] = script
-	// TODO update to server
+func (ss ScriptStore) Put(net, name, script string, callback func()) {
+	jquery.Ajax(map[string]interface{}{
+		"type": "PUT",
+		"url":  fmt.Sprintf("/script/%s/%s/", net, name),
+		"data": script,
+		jquery.SUCCESS: func(data string) {
+			ss[name] = script
+			console.Log("Successfully updated script", name, "@", net)
+			callback()
+		},
+		jquery.ERROR: func(info map[string]interface{}, textStatus, errorThrown string) {
+			console.Error("Failed to update script: HTTP", info["status"])
+			console.Error(info)
+		},
+	})
 }
 
 // Delete the script with the given name
-func (ss ScriptStore) Delete(name string) {
-	delete(ss, name)
-	// TODO update to server
+func (ss ScriptStore) Delete(net, name string, callback func()) {
+	jquery.Ajax(map[string]interface{}{
+		"type": "DELETE",
+		"url":  fmt.Sprintf("/script/%s/%s/", net, name),
+		jquery.SUCCESS: func() {
+			delete(ss, name)
+			console.Log("Successfully deleted script", name, "@", net)
+			callback()
+		},
+		jquery.ERROR: func(info map[string]interface{}, textStatus, errorThrown string) {
+			console.Error("Failed to delete script: HTTP", info["status"])
+			console.Error(info)
+		},
+	})
 }
