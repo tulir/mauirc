e2c889ebd7de3556ac157a1cc02ce6b40bf45a03
Tulir Asokan <tulir@maunium.net>
2016-11-08 18:56:12 +0200

    Add UI controls

--- js/networks.js
+++ js/networks.js
@@ -15,13 +15,21 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 class DataStore {
-	constructor() {
+	constructor(mauirc) {
+		this.mauirc = mauirc
 		this.networks = {}
+
+		mauirc.registerEventHandler("chanlist-channel:click", chan => {
+			this.getChannel(
+				chan.getAttribute("data-network"),
+				chan.getAttribute("data-name")
+			).open(chan)
+		})
 	}
 
 	getNetwork(name) {
 		if (!this.networks.hasOwnProperty(name)) {
-			this.putNetwork(new NetworkStore(name))
+			this.putNetwork(new NetworkStore(this.mauirc, name))
 		}
 		return this.networks[name]
 	}
@@ -38,11 +46,42 @@ class DataStore {
 		this.getNetwork(net).putChannel(name)
 	}
 
+	getChanlist() {
+		let chanlist = this.mauirc.container.find("#networks")
+		if (chanlist.length === 0) {
+			return undefined
+		}
+		return chanlist
+	}
+
+	getChatArea() {
+		let chat = this.mauirc.container.find(".chat-container > .chat")
+		if (chat.length === 0) {
+			return undefined
+		}
+		return chat
+	}
+
+	deselectChanlistEntries() {
+		let chanlist = this.getChanlist()
+		chanlist.find(".network.active").removeClass("active")
+		chanlist.find(".network .channel.active").removeClass("active")
+	}
+
+	closeChatAreas() {
+		let chat = this.getChatArea()
+		chat.find(".network-container:not(.hidden)").addClass("hidden")
+		chat.find(".network-container > .channel-container:not(.hidden)")
+			.addClass("hidden")
+	}
+
 	process(type, data) {
 		let net
 		switch(type) {
 		case "chandata":
-			let chan = new ChannelStore(data.name)
+			let chan = new ChannelStore(
+				this.mauirc, this.getNetwork(data.network), data.name
+			)
 			chan.topic = data.topic
 			chan.topicsetat = data.topicsetat
 			chan.topicsetby = data.topicsetby
@@ -67,7 +106,9 @@ class DataStore {
 }
 
 class NetworkStore {
-	constructor(name) {
+	constructor(mauirc, datastore, name) {
+		this.mauirc = mauirc
+		this.datastore = datastore
 		this.name = name
 		this.ip = ""
 		this.port = -1
@@ -80,9 +121,41 @@ class NetworkStore {
 		this.chanlist = []
 	}
 
+	getChanlistEntry() {
+		let chanlist = this.datastore.getChanlist()
+		if (chanlist === undefined) {
+			return
+		}
+
+		let network = chanlist.find(".network[data-name=" + this.name + "]")
+		if (network.length === 0) {
+			chanlist.append(Handlebars.templates["chanlist-network"], this)
+			network = chanlist.find(".network[data-name=" + this.name + "]")
+		}
+		return network
+	}
+
+	getChatAreaEntry() {
+		let chat = this.datastore.getChatArea()
+		if (chat === undefined) {
+			return
+		}
+
+		let network = chat.find(
+			".network-container[data-name=" + this.name + "]"
+		)
+		if (network.length === 0) {
+			chat.append(Handlebars.templates["chat-network"], this)
+			network = chat.find(
+				".network-container[data-name=" + this.name + "]"
+			)
+		}
+		return network
+	}
+
 	getChannel(name) {
 		if (!this.channels.hasOwnProperty(name)) {
-			this.putChannel(new ChannelStore(name))
+			this.putChannel(new ChannelStore(this.mauirc, this, name))
 		}
 
 		return this.channels[name]
@@ -94,12 +167,59 @@ class NetworkStore {
 }
 
 class ChannelStore {
-	constructor(name) {
+	constructor(mauirc, network, name) {
+		this.mauirc = mauirc
 		this.name = name
+		this.network = network
 		this.users = []
 		this.topic = ""
 		this.topicsetat = 0
 		this.topicsetby = ""
 		this.modes = undefined
+	}
+
+	getChanlistEntry() {
+		let network = this.network.getChanlistEntry()
+
+		let channel = network.find(
+			".channels > .channel[data-name=" + this.name + "]"
+		)
+		if (channel.length === 0) {
+			networks.append(Handlebars.templates["chanlist-channel"], {
+				name: this.name,
+				network: this.network.name
+			})
+			let channel = network.find(
+				".channels > .channel[data-name=" + this.name + "]"
+			)
+		}
+		return channel
+	}
+
+	getChatAreaEntry() {
+		let network = this.network.getChatAreaEntry()
+
+		let channel = chat.find(
+			".channel-container" +
+			"[data-name=" + this.name + "]"
+		)
+		if (network.length === 0) {
+			chat.append(Handlebars.templates["chat-channel"], this)
+			let channel = chat.find(
+				".channel-container" +
+				"[data-name=" + this.name + "]"
+			)
+		}
+		return channel
+	}
+
+	open() {
+		this.network.datastore.closeChatAreas()
+		this.getChanlistEntry().addClass("active")
+		this.getChatAreaEntry().removeClass("hidden")
+
+		this.network.datastore.deselectChanlistEntries()
+		this.network.getChanlistEntry().addClass("active")
+		this.network.getChatAreaEntry().removeClass("hidden")
 	}
 }
